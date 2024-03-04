9a3bd288203d8757902344480d677d9200be89fe
Vitali-Matteo <matteo7.vitali@mail.polimi.it>
2024-03-04 18:02:09 +0100

    Card class complete.

--- src/main/java/it/polimi/ingsw/gc19/Card/Card.java
+++ src/main/java/it/polimi/ingsw/gc19/Card/Card.java
@@ -1,4 +1,31 @@
 package it.polimi.ingsw.gc19.Card;
 
-public class Card {
+import it.polimi.ingsw.gc19.Station.Station;
+
+public abstract class Card{
+
+    private final String cardCode;
+
+    protected Card(String cardCode){
+        this.cardCode = cardCode;
+    }
+
+    public String getCardCode(){
+        return cardCode;
+    }
+
+    public abstract String getCardDescription();
+
+    public abstract int countPoints(Station station);
+
+    @Override
+    public boolean equals(Object obj){
+
+        if(obj instanceof Card cardObj){
+            return cardObj.cardCode.equals(this.cardCode);
+        }
+
+        return false;
+    }
+
 }
