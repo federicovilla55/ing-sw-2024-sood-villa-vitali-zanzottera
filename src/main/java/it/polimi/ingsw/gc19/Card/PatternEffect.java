75ce85761c46c8968db289c5de967d1531aeb621
Vitali-Matteo <matteo7.vitali@mail.polimi.it>
2024-03-04 17:57:17 +0100

    Added enum interfaces and classes for GoalEffect effects.

--- src/main/java/it/polimi/ingsw/gc19/Card/PatternEffect.java
+++ src/main/java/it/polimi/ingsw/gc19/Card/PatternEffect.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.gc19.Card;
 
-import it.polimi.ingsw.gc19.Other.Tuple;
+import it.polimi.ingsw.gc19.Enums.Direction;
 import it.polimi.ingsw.gc19.Station.Station;
 
 import java.util.List;
@@ -8,10 +8,10 @@ import java.util.List;
 public class PatternEffect implements GoalEffect{
 
     private final int cardValue;
-    private final List<Tuple<Integer, Integer>> direction;
+    private final List<Direction> direction;
     private final List<Symbol> requiredSymbol;
 
-    protected PatternEffect(int cardValue, List<Tuple<Integer, Integer>> direction, List<Symbol> requiredSymbol){
+    protected PatternEffect(int cardValue, List<Direction> direction, List<Symbol> requiredSymbol){
         this.cardValue = cardValue;
         this.direction = direction;
         this.requiredSymbol = requiredSymbol;
