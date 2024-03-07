93b67391103b06b48c58b94286173ea2d17a2522
Vitali-Matteo <matteo7.vitali@mail.polimi.it>
2024-03-07 14:12:13 +0100

    GoalEffect and PatternEffect are now completed

--- src/main/java/it/polimi/ingsw/gc19/Model/Card/PatternEffect.java
+++ src/main/java/it/polimi/ingsw/gc19/Model/Card/PatternEffect.java
@@ -1,26 +1,71 @@
 package it.polimi.ingsw.gc19.Model.Card;
 
 import it.polimi.ingsw.gc19.Model.Enums.Direction;
+import it.polimi.ingsw.gc19.Model.Enums.PlayableCardType;
 import it.polimi.ingsw.gc19.Model.Enums.Symbol;
 import it.polimi.ingsw.gc19.Model.Station.Station;
+import it.polimi.ingsw.gc19.Tuple.Tuple;
 
 import java.util.List;
 
 public class PatternEffect implements GoalEffect{
 
     private final int cardValue;
-    private final List<Direction> direction;
+    private final List<Tuple<Integer, Integer>> moves;
     private final List<Symbol> requiredSymbol;
 
-    protected PatternEffect(int cardValue, List<Direction> direction, List<Symbol> requiredSymbol){
+    protected PatternEffect(int cardValue, List<Tuple<Integer, Integer>> moves, List<Symbol> requiredSymbol){
         this.cardValue = cardValue;
-        this.direction = direction;
+        this.moves = moves;
         this.requiredSymbol = requiredSymbol;
     }
 
     @Override
-    public int countPoints(Station station, GoalCard card){
-        return 0; //TODO: implement method
+    public int countPoints(Station station){
+        boolean[][] usedCards = new boolean[40][40];
+        PlayableCard currentCard;
+        int numPattern;
+        boolean found;
+        int nextI, nextK, numOfCard;
+
+        for(int i = 0; i < 40; i++){
+            for(int k =0; k < 40; k++){
+                usedCards[i][k] = true;
+            }
+        }
+
+        numPattern = 0;
+
+        for(int i = 0; i < 40;i++){
+            for(int k = 0; k < 40; k++){
+                found = true;
+                nextI = i;
+                nextK = k;
+                numOfCard = 0;
+                while(found){
+                    try{
+                        currentCard = station.getCardSchema().getCard(nextI, nextK);
+                        if((currentCard.getCardType() != PlayableCardType.INITIAL) && (currentCard.getPermanentResources().getFirst() == requiredSymbol.get(numOfCard)) && usedCards[nextI][nextK]){
+                            nextI = nextI + moves.get(numOfCard).getX();
+                            nextK = nextK + moves.get(numOfCard).getY();
+                            usedCards[nextI][nextK] = false;
+                            numOfCard++;
+                        }
+                    }
+                    catch(Exception ignored){
+                        while(numOfCard > 0){
+                            usedCards[nextI][nextK] = true;
+                            nextI = nextI - moves.get(numOfCard).getX();
+                            nextK = nextK - moves.get(numOfCard).getY();
+                            numOfCard--;
+                        }
+                        found = false;
+                    };
+                }
+                numPattern++;
+            }
+        }
+        return numPattern * this.cardValue;
     }
 
     @Override
