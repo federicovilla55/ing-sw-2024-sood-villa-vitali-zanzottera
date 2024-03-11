a8f74bdce6b030847a41bc3a4e3efb4e933f53f7
Aryan Sood <aryansood972@gmail.com>
2024-03-11 14:40:43 +0100

    Create Controller Methods

--- src/main/java/it/polimi/ingsw/gc19/Controller/Controller.java
+++ src/main/java/it/polimi/ingsw/gc19/Controller/Controller.java
@@ -2,13 +2,36 @@ package it.polimi.ingsw.gc19.Controller;
 
 import it.polimi.ingsw.gc19.Model.Game.Game;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Controller {
     List<Game> activeGames;
     List<Game> nonActiveGames;
     Map<ClientPlayer, Game> clientToGame;
+    Controller()
+    {
+       clientToGame = new HashMap<ClientPlayer, Game>();
+    }
+    void CreateGame(ClientPlayer new_player, Game gameToCreate)
+    {
+        clientToGame.put(new_player,gameToCreate);
+        nonActiveGames.add(gameToCreate);
+    }
+
+    void JoinGame(ClientPlayer player, Game gameToJoin)
+    {
+        clientToGame.put(player,gameToJoin);
+    }
+
+    void ExitNonActiveGame(ClientPlayer player, Game gameToExit)
+    {
+
+    }
+
+    void ExitActiveGame(ClientPlayer player, Game gameToExit)
+    {
+
+    }
 
     
 }
