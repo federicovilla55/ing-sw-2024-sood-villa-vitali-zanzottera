66f6ce4a7907981b83a48a850eabd58c8a4d1465
Federico Villa <federico5.villa@mail.polimi.it>
2024-03-16 19:37:35 +0100

    Player Removal Test

--- src/test/java/it/polimi/ingsw/gc19/Model/Deck/DeckTest.java
+++ src/test/java/it/polimi/ingsw/gc19/Model/Deck/DeckTest.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.gc19.Model.Enums.PlayableCardType;
 import it.polimi.ingsw.gc19.Model.Game.Game;
 import it.polimi.ingsw.gc19.Model.Player.NameAlreadyInUseException;
 import it.polimi.ingsw.gc19.Model.Player.Player;
+import it.polimi.ingsw.gc19.Model.Player.PlayerNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -62,6 +63,23 @@ class DeckTest {
         game.createNewPlayer("Player1", null);
         assertEquals(1, game.getNumJoinedPlayer(), "Player count should be 1 after creating a player");
         assertThrows(NameAlreadyInUseException.class, () -> game.createNewPlayer("Player1", null), "Should throw NameAlreadyInUseException");
+    }
+
+    @Test
+    public void testPlayerRemoval() throws NameAlreadyInUseException, PlayerNotFoundException {
+        assertEquals(0, game.getNumJoinedPlayer(), "Initial player count should be 0");
+        game.createNewPlayer("Player1", null);
+        assertEquals(1, game.getNumJoinedPlayer(), "Player count should be 1 after creating a player");
+        game.createNewPlayer("Player2", null);
+        game.createNewPlayer("Player3", null);
+        assertEquals(3, game.getNumJoinedPlayer(), "Player count should be 3");
+        game.removePlayer(game.getPlayerByName("Player3"));
+        assertEquals(2, game.getNumJoinedPlayer());
+        game.removePlayer(game.getPlayerByName("Player2"));
+        assertEquals(1, game.getNumJoinedPlayer());
+        game.removePlayer(game.getPlayerByName("Player1"));
+        assertEquals(0, game.getNumJoinedPlayer());
+        assertThrows(PlayerNotFoundException.class, () -> game.getPlayerByName("Player1"));
     }
 
 }
