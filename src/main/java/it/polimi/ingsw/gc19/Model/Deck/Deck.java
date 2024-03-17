92c6416b5ea2b219fadb2147ad364ae32fd649de
Vitali-Matteo <matteo7.vitali@mail.polimi.it>
2024-03-17 15:13:12 +0100

    Added Java Doc.

--- src/main/java/it/polimi/ingsw/gc19/Model/Deck/Deck.java
+++ src/main/java/it/polimi/ingsw/gc19/Model/Deck/Deck.java
@@ -14,35 +14,36 @@ import java.util.stream.Stream;
 public class Deck<cardType extends Card>{
 
     private final ArrayList<cardType> cardsInDeck;
-    private  int initialLenOfDeck;
+    private  final int initialLenOfDeck;
 
+    /**
+     * This constructor creates a deck of cardType cards
+     * @param cardsInDeck stream of cards to put in deck
+     */
     public Deck(Stream<cardType> cardsInDeck) {
         this.cardsInDeck = cardsInDeck.collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
         this.initialLenOfDeck = this.cardsInDeck.size();
     }
 
-    private ArrayList<cardType> init(String filename) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        File f = new File(filename);
-
-        ArrayList<cardType> a;
-
-        a = objectMapper.readValue(f, new TypeReference<ArrayList<cardType>>(){});
-
-        return a;
-    }
-
+    /**
+     * This method shuffles deck
+     */
     public void shuffleDeck(){
         Collections.shuffle(this.cardsInDeck);
     }
 
-    ArrayList<cardType> getArray() {return cardsInDeck;}
-
+    /**
+     * This method return the initial len of deck
+     * @return the initial length of deck
+     */
     public int getInitialLenOfDeck(){
         return this.initialLenOfDeck;
     }
 
+    /**
+     * This method pick a random card from deck
+     * @return cardType card randomly chosen from the deck
+     */
     public cardType pickACard() throws EmptyDeckException{
         if(this.isEmpty()){
             throw new EmptyDeckException("You can't pick a card. Deck is empty!");
@@ -50,18 +51,35 @@ public class Deck<cardType extends Card>{
         return this.cardsInDeck.remove(new Random().nextInt(cardsInDeck.size()));
     }
 
+    /**
+     * This method insert a card in the deck
+     * @param card card to insert in deck
+     */
     public void insertCard(cardType card){
         this.cardsInDeck.addFirst(card);
     }
 
+    /**
+     * This method tells whether this deck is empty
+     * @return true if and only if the deck is empty
+     */
     public boolean isEmpty(){
         return this.cardsInDeck.isEmpty();
     }
 
+    /**
+     * This method checks if this deck contains a specific card
+     * @param cardToSearch the card to search for in deck
+     * @return true if the card is inside this deck
+     */
     public boolean cardIsInDeck(cardType cardToSearch){
         return this.cardsInDeck.contains(cardToSearch);
     }
 
+    /**
+     * This method return the current length of deck
+     * @return the current length of this deck
+     */
     public int numberOfCardInDeck(){
         return this.cardsInDeck.size();
     }
