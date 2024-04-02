a0cb99d0e7397c79af4ebd64843369e9c648a43b
aryan <aryansood972@gmail.com>
2024-04-02 14:11:28 +0200

    Updated mistake branch working on

--- src/main/java/it/polimi/ingsw/gc19/Networking/Client/TempFolderToTestServer/ClientRmiTest.java
+++ src/main/java/it/polimi/ingsw/gc19/Networking/Client/TempFolderToTestServer/ClientRmiTest.java
@@ -2,14 +2,30 @@ package it.polimi.ingsw.gc19.Networking.Client.TempFolderToTestServer;
 
 import it.polimi.ingsw.gc19.Networking.Client.VirtualClient;
 import it.polimi.ingsw.gc19.Networking.Server.Message.MessageToClient;
+import it.polimi.ingsw.gc19.Networking.Server.VirtualServer;
 
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class ClientRmiTest extends UnicastRemoteObject implements VirtualClient {
-    protected ClientRmiTest() throws RemoteException {
+    final VirtualServer server;
+    protected ClientRmiTest(VirtualServer server) throws RemoteException {
+        this.server = server;
     }
 
+    private void run() throws RemoteException {
+        this.server.NewConnection(this, "Aryan");
+    }
+
+    public static void main(String[] args)  throws RemoteException, NotBoundException {
+        Registry registry = LocateRegistry.getRegistry("127.0.0.1", 12122);
+        VirtualServer server = (VirtualServer) registry.lookup("RMIServer");
+
+        new ClientRmiTest(server).run();
+    }
     @Override
     public void GetMessage(MessageToClient message) {
 
