a6480697862e2c4e647b6db39cc2ad1bc7251ed0
Vitali-Matteo <matteo7.vitali@mail.polimi.it>
2024-04-06 17:55:15 +0200

    Added method to join first available game in MainController

--- src/main/java/it/polimi/ingsw/gc19/Controller/MainController.java
+++ src/main/java/it/polimi/ingsw/gc19/Controller/MainController.java
@@ -1,9 +1,6 @@
 package it.polimi.ingsw.gc19.Controller;
 
-import it.polimi.ingsw.gc19.Enums.CardOrientation;
-import it.polimi.ingsw.gc19.Enums.Color;
-import it.polimi.ingsw.gc19.Enums.Direction;
-import it.polimi.ingsw.gc19.Enums.PlayableCardType;
+import it.polimi.ingsw.gc19.Enums.*;
 import it.polimi.ingsw.gc19.Model.Game.Game;
 import it.polimi.ingsw.gc19.Model.Game.Player;
 import it.polimi.ingsw.gc19.Model.Tuple;
@@ -76,14 +73,12 @@ public class MainController {
             else{
                 player.update(new RefusedActionMessage(ErrorType.NAME_ALREADY_IN_USE,
                                                        "Game name " + gameName + " is already in use!"));
-                player.update(new AvailableGamesMessage(new ArrayList<>(findNonActiveGames())));
+                player.update(new AvailableGamesMessage(findAvailableGames()));
             }
         }
     }
 
-    public void registerToGame(ClientHandler player, String gameName) throws IllegalArgumentException{
-        //Possible exceptions: player not found, game not found, game already full, player already registered to other games...?
-        GameController gameControllerToJoin;
+    private void checkPlayer(ClientHandler player){
         synchronized(this.playerInfo) {
             if (!this.playerInfo.containsKey(player.getName())) {
                 throw new IllegalArgumentException();
@@ -92,6 +87,16 @@ public class MainController {
                 throw new IllegalArgumentException();
             }
         }
+    }
+
+    public void registerToFirstAvailableGame(ClientHandler player){
+        checkPlayer(player);
+        registerToGame(player, this.findAvailableGames().getFirst());
+    }
+
+    public void registerToGame(ClientHandler player, String gameName) throws IllegalArgumentException{
+        //Possible exceptions: player not found, game not found, game already full, player already registered to other games...?
+        checkPlayer(player);
         synchronized(this.gamesInfo) {
             if (!this.gamesInfo.containsKey(gameName)) {
                 throw new IllegalArgumentException();
@@ -100,19 +105,20 @@ public class MainController {
                 throw new IllegalArgumentException();
             }
         }
+        GameController gameControllerToJoin;
         gameControllerToJoin = this.gamesInfo.get(gameName).y();
         this.playerInfo.put(player.getName(), gameControllerToJoin);
         gameControllerToJoin.addClient(player.getName(), player); //Problems?
         //join game message?
     }
 
-    private Set<String> findNonActiveGames(){
+    private ArrayList<String> findAvailableGames(){
         synchronized(this.gamesInfo) {
             return this.gamesInfo.entrySet()
                                  .stream()
-                                 .filter(e -> e.getValue().x().getNumJoinedPlayer() == e.getValue().x().getNumPlayers())
+                                 .filter(e -> e.getValue().x().getGameState() == GameState.SETUP && e.getValue().x().getNumPlayers() != e.getValue().x().getNumJoinedPlayer())
                                  .map(Map.Entry::getKey)
-                                 .collect(Collectors.toSet());
+                                 .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
         }
     }
 
@@ -183,7 +189,7 @@ public class MainController {
             //clientHandler.sendMessageToClient();
         }
         if(!this.gamesInfo.containsKey(gameName)){
-            clientHandler.update(new AvailableGamesMessage(new ArrayList<>(findNonActiveGames())));
+            clientHandler.update(new AvailableGamesMessage(findAvailableGames()));
         }
         if(this.gamesInfo.get(gameName).x().getPlayers().stream().map(Player::getName).noneMatch(n -> n.equals(gameName))){
             //clientHandler.sendMessageToClient()
