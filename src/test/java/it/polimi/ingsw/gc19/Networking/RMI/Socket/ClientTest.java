c912d0904792ebc0c4922f9344ebc43c1532ddf1
Vitali-Matteo <matteo7.vitali@mail.polimi.it>
2024-04-08 13:33:06 +0200

    Added test logic in RMI network.

--- src/test/java/it/polimi/ingsw/gc19/Networking/RMI/Socket/ClientTest.java
+++ src/test/java/it/polimi/ingsw/gc19/Networking/RMI/Socket/ClientTest.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.gc19.Networking.RMI.Socket;
 
+import it.polimi.ingsw.gc19.Enums.Color;
 import it.polimi.ingsw.gc19.Networking.Client.VirtualClient;
 import it.polimi.ingsw.gc19.Networking.Server.Message.Action.AnswerToActionMessage;
 import it.polimi.ingsw.gc19.Networking.Server.Message.Chat.NotifyChatMessage;
@@ -9,6 +10,7 @@ import it.polimi.ingsw.gc19.Networking.Server.Message.GameHandling.GameHandlingM
 import it.polimi.ingsw.gc19.Networking.Server.Message.MessageToClient;
 import it.polimi.ingsw.gc19.Networking.Server.Message.Turn.TurnStateMessage;
 import it.polimi.ingsw.gc19.Networking.Server.VirtualGameServer;
+import it.polimi.ingsw.gc19.Networking.Server.VirtualMainServer;
 
 import java.io.Serializable;
 import java.rmi.*;
@@ -23,20 +25,23 @@ public class ClientTest{
 
     public static void main(String[] args) throws RemoteException, NotBoundException {
 
-        /*Registry registry = LocateRegistry.getRegistry("RMIServer", 12122);
-        VirtualGameServer virtualGameServer = (VirtualGameServer) registry.lookup("RMIServer");
+        Registry registry = LocateRegistry.getRegistry("RMIServer", 12122);
+        VirtualMainServer virtualMainServer = (VirtualMainServer) registry.lookup("RMIServer");
 
         Scanner scanner = new Scanner(System.in);
-        Client client1 = new Client(virtualGameServer, "Matteo");
-        Client client2 = new Client(virtualGameServer, "Mario");
+        Client client1 = new Client(virtualMainServer, "Matteo");
+        Client client2 = new Client(virtualMainServer, "Mario");
         client1.connect("Matteo");
         client2.connect("Mario");
         client1.newGame("Matteo", "Game");
+        Registry gameRegistry1 = LocateRegistry.getRegistry("Game_Matteo", 12122);
+        VirtualGameServer virtualGameServer1 = (VirtualGameServer) registry.lookup("Game_Matteo");
+        virtualGameServer1.chooseColor(Color.BLUE);
         client2.joinGame("Mario", "Game");
         //client2.joinGame("Mario", "Game1");
-        client1.sendChatMessage("Matteo", new ArrayList<>(List.of("Mario", "Matteo")), "Ciao!!!");
-        client1.disconnect("Matteo");
-        client1.reconnect("Matteo", "Game");*/
+        //client1.sendChatMessage("Matteo", new ArrayList<>(List.of("Mario", "Matteo")), "Ciao!!!");
+        //client1.disconnect("Matteo");
+        //client1.reconnect("Matteo", "Game");
 
 
     }
@@ -44,21 +49,14 @@ public class ClientTest{
 }
 
 class Client extends UnicastRemoteObject implements VirtualClient, Serializable{
-    protected Client() throws RemoteException {
-    }
 
-    @Override
-    public void pushUpdate(MessageToClient message) throws RemoteException {
-
-    }
-
-    /*private final VirtualGameServer virtualGameServer;
+    private final VirtualMainServer virtualMainServer;
 
     private final String name;
 
-    public Client(VirtualGameServer virtualGameServer, String name) throws RemoteException {
+    public Client(VirtualMainServer virtualMainServer, String name) throws RemoteException {
         super();
-        this.virtualGameServer = virtualGameServer;
+        this.virtualMainServer = virtualMainServer;
         this.name = name;
     }
 
@@ -70,24 +68,24 @@ class Client extends UnicastRemoteObject implements VirtualClient, Serializable{
     public void pushUpdate(MessageToClient message) {
         //System.out.println("FOUND SOMETHING!!!!!!");
         System.out.println(name + " received " + message.getClass() + "  ->  " + message);
-        /*if(message instanceof NotifyChatMessage){
+        if(message instanceof NotifyChatMessage){
             System.out.println(((NotifyChatMessage) message).getMessage());
         }
     }
 
     public void connect(String name) throws RemoteException {
-        this.virtualGameServer.newConnection(this, name);
+        this.virtualMainServer.newConnection(this, name);
     }
 
     public void newGame(String nick, String gameName) throws RemoteException{
-        this.virtualGameServer.createGame(this, gameName, 2);
+        this.virtualMainServer.createGame(this, gameName, 2);
     }
 
     public void joinGame(String nick, String game) throws RemoteException {
-        this.virtualGameServer.joinGame(this, game);
+        this.virtualMainServer.joinGame(this, game);
     }
 
-    public void sendChatMessage(String nick, ArrayList<String> receivers, String message) throws RemoteException {
+    /*public void sendChatMessage(String nick, ArrayList<String> receivers, String message) throws RemoteException {
         this.virtualGameServer.sendChatMessage(this, receivers, message);
     }
 
