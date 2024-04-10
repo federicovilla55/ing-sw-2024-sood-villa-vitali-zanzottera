4fd755a9c60dc365e82cc3ff0bed6b6458776635
Vitali-Matteo <matteo7.vitali@mail.polimi.it>
2024-04-10 14:28:20 +0200

    Added JavaDocs for Server

--- src/main/java/it/polimi/ingsw/gc19/Networking/Server/Server.java
+++ src/main/java/it/polimi/ingsw/gc19/Networking/Server/Server.java
@@ -2,6 +2,10 @@ package it.polimi.ingsw.gc19.Networking.Server;
 
 import it.polimi.ingsw.gc19.Controller.MainController;
 
+/**
+ * This abstract class represents a generic server. In other words,
+ * some "network interfaces" of the application
+ */
 public abstract class Server{
 
     protected final MainController mainController;
