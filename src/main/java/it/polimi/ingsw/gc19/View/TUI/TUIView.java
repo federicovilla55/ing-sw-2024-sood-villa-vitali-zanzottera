425752c57a0ea82e94ce2a310ea734f6f1194a5c
Marco Zanzottera <marcozanzottera@e.email>
2024-04-28 16:22:08 +0200

    test: add tableTUIView and relative test

--- src/main/java/it/polimi/ingsw/gc19/View/TUI/TUIView.java
+++ src/main/java/it/polimi/ingsw/gc19/View/TUI/TUIView.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.gc19.View.TUI;
 
+import it.polimi.ingsw.gc19.Enums.CardOrientation;
 import it.polimi.ingsw.gc19.Enums.CornerPosition;
 import it.polimi.ingsw.gc19.Enums.PlayableCardType;
 import it.polimi.ingsw.gc19.Enums.Symbol;
@@ -9,6 +10,7 @@ import it.polimi.ingsw.gc19.Utils.Tuple;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class TUIView {
@@ -16,6 +18,7 @@ public class TUIView {
     /**
      * This method prints to terminal a matrix of strings, each string represents two UTF-8 wide character(s),
      * Making all lines aligned. Make sure to use monospace supported fonts, line Noto Sans Mono + Noto Color Emoji
+     *
      * @param cardTUIView the matrix of strings to print
      */
     public void printTUIView(String[][] cardTUIView) {
@@ -25,6 +28,25 @@ public class TUIView {
             }
             System.out.println();
         }
+    }
+
+    public String[] textTUIView(String text) {
+        List<String> res = new ArrayList<>();
+        boolean startOfString = true;
+        for (Character c : text.toCharArray()) {
+            if (startOfString) {
+                res.add("" + c);
+                startOfString = false;
+            } else {
+                res.set(res.size() - 1, res.get(res.size() - 1) + c);
+                startOfString = true;
+            }
+        }
+        if (!startOfString) {
+            res.set(res.size() - 1, res.get(res.size() - 1) + " ");
+        }
+
+        return res.toArray(String[]::new);
     }
 
     public String[][] cardTUIView(PlayableCard card) {
@@ -66,11 +88,11 @@ public class TUIView {
                 res[row][2] = res[row][2].concat(resource.stringEmoji());
                 row++;
             }
-            if(permanentResources.size() <= 2) {
+            if (permanentResources.size() <= 2) {
                 res[2][2] = res[2][2].concat("  ");
-                if(permanentResources.size() <= 1) {
+                if (permanentResources.size() <= 1) {
                     res[0][2] = res[0][2].concat("  ");
-                    if(permanentResources.isEmpty()) {
+                    if (permanentResources.isEmpty()) {
                         res[1][2] = res[1][2].concat("  ");
                     }
                 }
@@ -81,7 +103,7 @@ public class TUIView {
         for (Tuple<Integer, Integer> pos : List.of(new Tuple<>(1, 0), new Tuple<>(1, 4),
                 new Tuple<>(0, 1), new Tuple<>(1, 1), new Tuple<>(2, 1),
                 new Tuple<>(0, 3), new Tuple<>(1, 3), new Tuple<>(2, 3))) {
-            res[pos.x()][pos.y()] = res[pos.x()][pos.y()].concat("  " + res[pos.x()][pos.y()]);
+            res[pos.x()][pos.y()] = res[pos.x()][pos.y()].concat("  ");
         }
         //add ANSI code to reset background color
         for (String[] strings : res) {
@@ -92,7 +114,41 @@ public class TUIView {
         return res;
     }
 
-    public String[][] playerAreaTUIView(List<Tuple<PlayableCard,Tuple<Integer,Integer>>> placedCardSequence) {
+    public String[][] deckSeedTUIView(Symbol deckSeed) {
+        // create matrix of empty strings, each representing a single unicode character to display in console
+        String[][] res = new String[3][5];
+        for (String[] strings : res) {
+            Arrays.fill(strings, "");
+        }
+        // set correct card color
+        for (String[] strings : res) {
+            for (int i = 0; i < strings.length; i++) {
+                strings[i] = strings[i].concat(deckSeed.stringColor());
+            }
+        }
+        //add to every corner the correct emoji (empty corner)
+        for (CornerPosition cp : CornerPosition.values()) {
+            res[cp.getX() * 2][cp.getY() * 4] = res[cp.getX() * 2][cp.getY() * 4].concat("\uD83D\uDFE8");
+        }
+        //add visible permanent resources
+        res[1][2] = res[1][2].concat(deckSeed.stringEmoji());
+        // add space character where it is certain to not have any symbol
+        for (Tuple<Integer, Integer> pos : List.of(new Tuple<>(1, 0), new Tuple<>(1, 4),
+                new Tuple<>(0, 1), new Tuple<>(1, 1), new Tuple<>(2, 1),
+                new Tuple<>(0, 3), new Tuple<>(1, 3), new Tuple<>(2, 3),
+                new Tuple<>(0, 2), new Tuple<>(2, 2))) {
+            res[pos.x()][pos.y()] = res[pos.x()][pos.y()].concat("  ");
+        }
+        //add ANSI code to reset background color
+        for (String[] strings : res) {
+            for (int i = 0; i < strings.length; i++) {
+                strings[i] = strings[i].concat("\u001B[0m");
+            }
+        }
+        return res;
+    }
+
+    public String[][] playerAreaTUIView(List<Tuple<PlayableCard, Tuple<Integer, Integer>>> placedCardSequence) {
         //determine dimension of the matrix
         int h;
         int w;
@@ -113,8 +169,8 @@ public class TUIView {
         //  w*5-(w-1)=5w-w+1=4w+1
         // height: for every card 3 cells, minus a cell for every card minus 1, simplifying
         //  h*3-(h-1)=3h-h+1=2h+1
-        h = 2*h+1;
-        w = 4*w+1;
+        h = 2 * h + 1;
+        w = 4 * w + 1;
 
         // create matrix of "  " strings, each representing two characters to display in console
         String[][] res = new String[h][w];
@@ -124,22 +180,22 @@ public class TUIView {
 
         //iterate over all cards and add them to the matrix. Card order is important:
         //new cards overwrite angles of older cards, so they have to be passed in place order
-        for(Tuple<PlayableCard,Tuple<Integer,Integer>> cardAndPosition
+        for (Tuple<PlayableCard, Tuple<Integer, Integer>> cardAndPosition
                 : placedCardSequence) {
             //use firstRow and firstCol to determine position relative to the matrix
             int relX = cardAndPosition.y().x() - firstRow;
             int relY = cardAndPosition.y().y() - firstCol;
 
             //change relX and relY to coordinates to the characters coords
-            relX=2*relX;
-            relY=4*relY;
+            relX = 2 * relX;
+            relY = 4 * relY;
 
             String[][] cardTUIView = this.cardTUIView(cardAndPosition.x());
 
             //iterate on the matrix and place the card
-            for(int i=0; i < cardTUIView.length; i++) {
-                for(int j=0; j < cardTUIView[0].length; j++) {
-                    res[relX+i][relY+j] = cardTUIView[i][j];
+            for (int i = 0; i < cardTUIView.length; i++) {
+                for (int j = 0; j < cardTUIView[0].length; j++) {
+                    res[relX + i][relY + j] = cardTUIView[i][j];
                 }
             }
 
@@ -148,13 +204,137 @@ public class TUIView {
         return res;
     }
 
-    public String[][] tableTUIView() {
+    public String[][] tableTUIView(PlayableCard resource1, PlayableCard resource2, PlayableCard gold1, PlayableCard gold2, Symbol resourceDeckSeed, Symbol goldDeckSeed) {
         // create matrix of "  " strings, each representing a single unicode character to display in console
-        String[][] res = new String[12][25];
+        String[][] res = new String[14][25];
         for (String[] strings : res) {
-            Arrays.fill(strings, "aa");
+            Arrays.fill(strings, "  ");
         }
 
+        List<String> labels = List.of("res_1", "res_2", "gold_1", "gold_2");
+        List<PlayableCard> playableCards = Arrays.asList(resource1, resource2, gold1, gold2);
+        List<Tuple<Integer, Integer>> initialCoordsInString = List.of(
+                new Tuple<>(2, 4),
+                new Tuple<>(2, 12),
+                new Tuple<>(8, 4),
+                new Tuple<>(8, 12)
+        );
+
+        Iterator<String> labelsIterator = labels.iterator();
+        Iterator<PlayableCard> cardsIterator = playableCards.iterator();
+        Iterator<Tuple<Integer, Integer>> coordsIterator = initialCoordsInString.iterator();
+
+        while (labelsIterator.hasNext() && cardsIterator.hasNext() && coordsIterator.hasNext()) {
+
+            String label = labelsIterator.next();
+            PlayableCard card = cardsIterator.next();
+            Tuple<Integer, Integer> coords = coordsIterator.next();
+
+            //print label for command line commands over printed card
+            String[] textTUIView = textTUIView(label + ":");
+            for (int i = 0; i < textTUIView.length; i++) {
+                res[coords.x() - 1][coords.y() + i] = textTUIView[i];
+            }
+            if (card != null) {
+                card.setCardState(CardOrientation.UP);
+                String[][] cardTUIView = cardTUIView(card);
+
+                //print card
+                for (int i = 0; i < cardTUIView.length; i++) {
+                    for (int j = 0; j < cardTUIView[i].length; j++) {
+                        res[coords.x() + i][coords.y() + j] = cardTUIView[i][j];
+                    }
+                }
+                //print card code under the printed card
+                textTUIView = textTUIView(card.getCardCode());
+                for (int i = 0; i < textTUIView.length; i++) {
+                    res[coords.x() + 3][coords.y() + i] = textTUIView[i];
+                }
+            } else {
+                String[][] emptyCardTUIView = emptyCardTUIView();
+                //print empty card placeholder
+                for (int i = 0; i < emptyCardTUIView.length; i++) {
+                    for (int j = 0; j < emptyCardTUIView[i].length; j++) {
+                        res[coords.x() + i][coords.y() + j] = emptyCardTUIView[i][j];
+                    }
+                }
+            }
+        }
+
+        //print label for command line commands over printed deck
+        String[] textTUIView = textTUIView("res_deck:");
+        for (int i = 0; i < textTUIView.length; i++) {
+            res[2 - 1][20 + i] = textTUIView[i];
+        }
+        if (resourceDeckSeed != null) {
+            // print deck
+            String[][] deckSeedTUIView = deckSeedTUIView(resourceDeckSeed);
+            for (int i = 0; i < deckSeedTUIView.length; i++) {
+                for (int j = 0; j < deckSeedTUIView[i].length; j++) {
+                    res[2 + i][20 + j] = deckSeedTUIView[i][j];
+                }
+            }
+        } else {
+            String[][] emptyCardTUIView = emptyCardTUIView();
+            //print empty card placeholder
+            for (int i = 0; i < emptyCardTUIView.length; i++) {
+                for (int j = 0; j < emptyCardTUIView[i].length; j++) {
+                    res[2 + i][20 + j] = emptyCardTUIView[i][j];
+                }
+            }
+        }
+
+
+        //print label for command line commands over printed deck
+        textTUIView = textTUIView("gold_deck:");
+        for (int i = 0; i < textTUIView.length; i++) {
+            res[8 - 1][20 + i] = textTUIView[i];
+        }
+        if (goldDeckSeed != null) {
+            // print deck
+            String[][] deckSeedTUIView = deckSeedTUIView(goldDeckSeed);
+            for (int i = 0; i < deckSeedTUIView.length; i++) {
+                for (int j = 0; j < deckSeedTUIView[i].length; j++) {
+                    res[8 + i][20 + j] = deckSeedTUIView[i][j];
+                }
+            }
+        } else {
+            String[][] emptyCardTUIView = emptyCardTUIView();
+            //print empty card placeholder
+            for (int i = 0; i < emptyCardTUIView.length; i++) {
+                for (int j = 0; j < emptyCardTUIView[i].length; j++) {
+                    res[8 + i][20 + j] = emptyCardTUIView[i][j];
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private String[][] emptyCardTUIView() {
+        // create matrix of empty strings, each representing a single unicode character to display in console
+        String[][] res = new String[3][5];
+        for (String[] strings : res) {
+            Arrays.fill(strings, "");
+        }
+        // set background color to grey
+        for (String[] strings : res) {
+            for (int i = 0; i < strings.length; i++) {
+                strings[i] = strings[i].concat("\u001b[48;5;245m");
+            }
+        }
+        // add space characters
+        for (String[] strings : res) {
+            for (int i = 0; i < strings.length; i++) {
+                strings[i] = strings[i].concat("  ");
+            }
+        }
+        //add ANSI code to reset background color
+        for (String[] strings : res) {
+            for (int i = 0; i < strings.length; i++) {
+                strings[i] = strings[i].concat("\u001B[0m");
+            }
+        }
         return res;
     }
 }
--- src/test/java/it/polimi/ingsw/gc19/View/TUI/TUICardViewTest.java
+++ src/test/java/it/polimi/ingsw/gc19/View/TUI/TUICardViewTest.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.gc19.View.TUI;
 
 import it.polimi.ingsw.gc19.Controller.JSONParser;
 import it.polimi.ingsw.gc19.Enums.CardOrientation;
+import it.polimi.ingsw.gc19.Enums.Symbol;
 import it.polimi.ingsw.gc19.Model.Card.GoalCard;
 import it.polimi.ingsw.gc19.Model.Card.PlayableCard;
 import it.polimi.ingsw.gc19.Utils.Tuple;
@@ -30,9 +31,9 @@ public class TUICardViewTest {
     public void testPlayableCardTUIView() {
         String[][] cardTUIView;
         PlayableCard card;
-        for(String type : List.of("resource_", "gold_")) {
-            for(Integer i = 1; i<=40; i++) {
-                String cardString = type+new DecimalFormat("00").format(i);
+        for (String type : List.of("resource_", "gold_")) {
+            for (Integer i = 1; i <= 40; i++) {
+                String cardString = type + new DecimalFormat("00").format(i);
                 System.out.println(cardString + ":");
                 card = stringPlayableCardHashMap.get(cardString);
                 cardTUIView = tuiView.cardTUIView(card);
@@ -44,8 +45,8 @@ public class TUICardViewTest {
                 System.out.println();
             }
         }
-        for(Integer i = 1; i<=6; i++) {
-            String cardString = "initial_"+new DecimalFormat("00").format(i);
+        for (Integer i = 1; i <= 6; i++) {
+            String cardString = "initial_" + new DecimalFormat("00").format(i);
             System.out.println(cardString + ":");
             card = stringPlayableCardHashMap.get(cardString);
             cardTUIView = tuiView.cardTUIView(card);
@@ -91,9 +92,29 @@ public class TUICardViewTest {
     public void testTableTUIView() {
         String[][] tableTUIView;
 
-        tableTUIView = tuiView.tableTUIView();
+        tableTUIView = tuiView.tableTUIView(
+                stringPlayableCardHashMap.get("resource_01"),
+                stringPlayableCardHashMap.get("resource_11"),
+                stringPlayableCardHashMap.get("gold_21"),
+                stringPlayableCardHashMap.get("gold_31"),
+                Symbol.INSECT,
+                Symbol.ANIMAL
+        );
 
         tuiView.printTUIView(tableTUIView);
+
+        tableTUIView = tuiView.tableTUIView(
+                stringPlayableCardHashMap.get("resource_01"),
+                null,
+                null,
+                stringPlayableCardHashMap.get("gold_31"),
+                Symbol.INSECT,
+                null
+        );
+
+        tuiView.printTUIView(tableTUIView);
+
+
     }
 
 }
