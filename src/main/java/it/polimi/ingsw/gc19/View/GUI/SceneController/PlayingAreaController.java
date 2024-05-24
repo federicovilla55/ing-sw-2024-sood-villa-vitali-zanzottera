b24bdbaa4430540cacf4372d97b587f09e8884a9
Marco Zanzottera <marcozanzottera@e.email>
2024-05-24 10:26:29 +0200

    initial PlayingArea fxml and controller

--- src/main/java/it/polimi/ingsw/gc19/View/GUI/SceneController/PlayingAreaController.java
+++ src/main/java/it/polimi/ingsw/gc19/View/GUI/SceneController/PlayingAreaController.java
@@ -1,45 +1,41 @@
 package it.polimi.ingsw.gc19.View.GUI.SceneController;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import it.polimi.ingsw.gc19.Enums.CardOrientation;
 import it.polimi.ingsw.gc19.Model.Card.PlayableCard;
-import it.polimi.ingsw.gc19.Networking.Client.ClientInterface;
-import it.polimi.ingsw.gc19.Networking.Client.Configuration.Configuration;
 import it.polimi.ingsw.gc19.Utils.Tuple;
 import it.polimi.ingsw.gc19.View.ClientController.ClientController;
-import it.polimi.ingsw.gc19.View.ClientController.Disconnect;
-import it.polimi.ingsw.gc19.View.ClientController.ViewState;
 import it.polimi.ingsw.gc19.View.Command.CommandParser;
-import it.polimi.ingsw.gc19.View.GUI.SceneStatesEnum;
-import it.polimi.ingsw.gc19.View.Listeners.GameHandlingListeners.GameHandlingEvents;
-import it.polimi.ingsw.gc19.View.Listeners.GameHandlingListeners.GameHandlingListener;
 import it.polimi.ingsw.gc19.View.Listeners.ListenerType;
-import it.polimi.ingsw.gc19.View.Listeners.StateListener.StateListener;
-import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
 public class PlayingAreaController extends AbstractController {
 
+    private static final double PIXEL_WIDTH = 807.04;
+    private static final double PIXEL_HEIGHT = 328.04;
+
+    @FXML
+    private StackPane centerPane;
+
     @FXML
     private GridPane cardGrid;
 
-    public PlayingAreaController(ClientController controller, CommandParser parser, Stage stage){
+    public PlayingAreaController(ClientController controller, CommandParser parser, Stage stage) {
         super(controller, parser, stage);
 
         super.getClientController().getListenersManager().attachListener(ListenerType.GAME_HANDLING_EVENTS_LISTENER, this);
@@ -56,26 +52,67 @@ public class PlayingAreaController extends AbstractController {
     public void setCardGrid(List<Tuple<PlayableCard, Tuple<Integer, Integer>>> placedCardSequence) {
         System.out.println("set up cards in grid");
 
+        centerPane.setPrefSize(1920,1080);
+
         //find first and last row with a card placed
         int firstRow = placedCardSequence.stream().mapToInt(x -> x.y().x()).min().orElse(0);
         int firstCol = placedCardSequence.stream().mapToInt(x -> x.y().y()).min().orElse(0);
+        int lastRow = placedCardSequence.stream().mapToInt(x -> x.y().x()).max().orElse(0);
+        int lastCol = placedCardSequence.stream().mapToInt(x -> x.y().y()).max().orElse(0);
 
-        cardGrid.setGridLinesVisible(false);
+        cardGrid.getChildren().clear();
 
-        for(Tuple<PlayableCard, Tuple<Integer, Integer>> card : placedCardSequence) {
+        DoubleProperty widthProperty = new SimpleDoubleProperty();
+        DoubleProperty heightProperty = new SimpleDoubleProperty();
+        DoubleProperty cellWidthProperty = new SimpleDoubleProperty();
+        DoubleProperty cellHeightProperty = new SimpleDoubleProperty();
+        DoubleProperty cardWidthProperty = new SimpleDoubleProperty();
+        DoubleProperty cardHeightProperty = new SimpleDoubleProperty();
 
-            StackPane.setMargin(cardGrid, new Insets(44, 36, 44, 36));
+        double magnification = 1.0;
+
+        widthProperty.bind(Bindings.min(
+                centerPane.widthProperty().multiply(magnification), centerPane.heightProperty().multiply(PIXEL_WIDTH).divide(PIXEL_HEIGHT).multiply(magnification)));
+        heightProperty.bind(Bindings.min(
+                centerPane.heightProperty().multiply(magnification), centerPane.widthProperty().multiply(PIXEL_HEIGHT).divide(PIXEL_WIDTH).multiply(magnification)));
+
+        cellWidthProperty.bind(widthProperty.divide(lastCol - firstCol + 3));
+        cellHeightProperty.bind(heightProperty.divide(lastRow - firstRow + 3));
+
+        cardWidthProperty.bind(cellWidthProperty.multiply(1.28));
+        cardHeightProperty.bind(cellHeightProperty.multiply(1.694915254));
+
+        for (int i = firstRow - 1; i <= lastRow + 1; i++) {
+            RowConstraints row = new RowConstraints();
+            cardGrid.getRowConstraints().add(row);
+            row.setValignment(VPos.CENTER);
+            row.prefHeightProperty().bind(cellHeightProperty);
+            row.minHeightProperty().bind(row.prefHeightProperty());
+            row.maxHeightProperty().bind(row.prefHeightProperty());
+        }
+
+        for (int i = firstCol - 1; i <= lastCol + 1; i++) {
+            ColumnConstraints col = new ColumnConstraints();
+            cardGrid.getColumnConstraints().add(col);
+            col.setHalignment(HPos.CENTER);
+            col.prefWidthProperty().bind(cellWidthProperty);
+            col.minWidthProperty().bind(col.prefWidthProperty());
+            col.maxWidthProperty().bind(col.prefWidthProperty());
+        }
+
+        cardGrid.setGridLinesVisible(true);
+
+        for (Tuple<PlayableCard, Tuple<Integer, Integer>> card : placedCardSequence) {
 
             ImageView cardImage = new ImageView(new Image(Objects.requireNonNull(getClass().getResource("/images/" + card.x().getCardCode() + "_" +
                     (card.x().getCardOrientation() == CardOrientation.UP ? "front" : "back")
                     + ".jpg")).toExternalForm()));
 
-            GridPane.setMargin(cardImage, new Insets(-40, -32, -40, -32));
+            cardImage.fitWidthProperty().bind(cardWidthProperty);
+            cardImage.fitHeightProperty().bind(cardHeightProperty);
 
-            cardImage.setPreserveRatio(true);
-            cardImage.setFitWidth(300);
-
-            cardGrid.add(cardImage, card.y().y() - firstCol, card.y().x() - firstRow);
+            cardGrid.add(cardImage, card.y().y() - firstCol + 1, card.y().x() - firstRow + 1);
         }
+
     }
 }
