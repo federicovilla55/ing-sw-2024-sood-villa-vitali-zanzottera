801b897ea61a3fe3a76e56882349d10a2c1d35d7
Vitali-Matteo <matteo7.vitali@mail.polimi.it>
2024-06-09 21:04:18 +0200

    Fix bugs

--- src/main/java/it/polimi/ingsw/gc19/Costants/ImportantConstants.java
+++ src/main/java/it/polimi/ingsw/gc19/Costants/ImportantConstants.java
@@ -1,7 +1,7 @@
 package it.polimi.ingsw.gc19.Costants;
 
 public class ImportantConstants{
-    public static final int gridDimension = 50;
-    public static final String pathToPlayableCardFileJSON = "it/polimi/ingsw/gc19/Cards/JSON Description/playable_cards.json";
-    public static final String pathToGoalCardFileJSON = "it/polimi/ingsw/gc19/Cards/JSON Description/goal_cards.json";
+    public static final int gridDimension = 80;
+    public static final String PATH_TO_PLAYABLE_CARD_FILE_JSON = "it/polimi/ingsw/gc19/Cards/JSON Description/playable_cards.json";
+    public static final String PATH_TO_GOAL_CARD_FILE_JSON = "it/polimi/ingsw/gc19/Cards/JSON Description/goal_cards.json";
 }
