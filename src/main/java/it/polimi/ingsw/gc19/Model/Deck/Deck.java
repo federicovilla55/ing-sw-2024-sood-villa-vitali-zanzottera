08f33b0b1412d73b45ce009900de26be28a60bbf
Vitali-Matteo <matteo7.vitali@mail.polimi.it>
2024-06-10 13:59:00 +0200

    Suppressed unused methods in Deck.java

--- src/main/java/it/polimi/ingsw/gc19/Model/Deck/Deck.java
+++ src/main/java/it/polimi/ingsw/gc19/Model/Deck/Deck.java
@@ -9,7 +9,6 @@ import java.util.stream.Stream;
 public class Deck<cardType extends Card>{
 
     private final ArrayList<cardType> cardsInDeck;
-    private final int initialLenOfDeck;
 
     /**
      * This constructor creates a deck of cardType cards
@@ -17,7 +16,6 @@ public class Deck<cardType extends Card>{
      */
     public Deck(Stream<cardType> cardsInDeck) {
         this.cardsInDeck = cardsInDeck.collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
-        this.initialLenOfDeck = this.cardsInDeck.size();
     }
 
     /**
@@ -25,14 +23,6 @@ public class Deck<cardType extends Card>{
      */
     public void shuffleDeck(Random random){
         Collections.shuffle(this.cardsInDeck, random);
-    }
-
-    /**
-     * This method return the initial len of deck
-     * @return the initial length of deck
-     */
-    public int getInitialLenOfDeck(){
-        return this.initialLenOfDeck;
     }
 
     /**
@@ -47,14 +37,6 @@ public class Deck<cardType extends Card>{
     }
 
     /**
-     * This method insert a card in the deck
-     * @param card card to insert in deck
-     */
-    public void insertCard(cardType card){
-        this.cardsInDeck.addFirst(card);
-    }
-
-    /**
      * This method tells whether this deck is empty
      * @return true if and only if the deck is empty
      */
@@ -62,28 +44,11 @@ public class Deck<cardType extends Card>{
         return this.cardsInDeck.isEmpty();
     }
 
-    /**
-     * This method checks if this deck contains a specific card
-     * @param cardToSearch the card to search for in deck
-     * @return true if the card is inside this deck
-     */
-    public boolean cardIsInDeck(cardType cardToSearch){
-        return this.cardsInDeck.contains(cardToSearch);
-    }
-
     public Optional<cardType> getNextCard(){
         if(!this.cardsInDeck.isEmpty()){
             return Optional.of(this.cardsInDeck.getFirst());
         }
         return Optional.empty();
-    }
-
-    /**
-     * This method return the current length of deck
-     * @return the current length of this deck
-     */
-    public int numberOfCardInDeck(){
-        return this.cardsInDeck.size();
     }
 
 }
