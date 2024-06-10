b7444439ea8ee0bd4383f06700952142064e961f
Vitali-Matteo <matteo7.vitali@mail.polimi.it>
2024-06-10 16:36:03 +0200

    Fix bugs in ImportantConstants

--- src/main/java/it/polimi/ingsw/gc19/Costants/ImportantConstants.java
+++ src/main/java/it/polimi/ingsw/gc19/Costants/ImportantConstants.java
@@ -4,7 +4,7 @@ package it.polimi.ingsw.gc19.Costants;
  * Enum of important constants for model and controller
  */
 public class ImportantConstants{
-    public static final int gridDimension = 50;
+    public static final int gridDimension = 81;
     public static final String PATH_TO_PLAYABLE_CARD_FILE_JSON = "it/polimi/ingsw/gc19/Cards/JSON Description/playable_cards.json";
     public static final String PATH_TO_GOAL_CARD_FILE_JSON = "it/polimi/ingsw/gc19/Cards/JSON Description/goal_cards.json";
 }
