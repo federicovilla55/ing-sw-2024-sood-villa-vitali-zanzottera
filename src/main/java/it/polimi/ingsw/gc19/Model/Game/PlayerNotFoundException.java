e348c1863c656f790e6518353cce959220051915
Marco Zanzottera <marcozanzottera@e.email>
2024-06-11 11:08:19 +0200

    Extend test coverage

--- src/main/java/it/polimi/ingsw/gc19/Model/Game/PlayerNotFoundException.java
+++ src/main/java/it/polimi/ingsw/gc19/Model/Game/PlayerNotFoundException.java
@@ -1,13 +1,8 @@
 package it.polimi.ingsw.gc19.Model.Game;
 
-public class PlayerNotFoundException extends RuntimeException{
+public class PlayerNotFoundException extends RuntimeException {
 
-    public PlayerNotFoundException(String message){
+    public PlayerNotFoundException(String message) {
         super(message);
     }
-
-    public PlayerNotFoundException(Exception ex){
-        super(ex);
-    }
-
 }
